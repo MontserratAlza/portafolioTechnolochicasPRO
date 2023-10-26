50fb00154d7a8cd6608527fbb6de31caf4d7ad00
MontserratAlza <luis.carl64@gmail.com>
2023-10-26 09:41:38 -0600

    Cambios d eimagenes de los proyectos

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ let typewriter = new Typewriter(app, {
  
 typewriter
   .pauseFor(2500)
-  .typeString('Desarrollo y diseño de Sitios Web, Software y Aplicaciones.')
+  .typeString('Desarrollo y diseño de Sitios Web y Software')
   .pauseFor(200)
   .deleteChars(10)
   .start();
