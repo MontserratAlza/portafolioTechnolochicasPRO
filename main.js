299d61ab6466f67b9953cf31e5f56fc276d3ae27
MontserratAlza <luis.carl64@gmail.com>
2023-11-30 09:47:09 -0600

    ultimo portafolio

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ let typewriter = new Typewriter(app, {
  
 typewriter
   .pauseFor(2500)
-  .typeString('Desarrollo y diseño de Sitios Web y Software')
+  .typeString('Desarrollo y diseño de Sitios Web, Software Frontend')
   .pauseFor(200)
   .deleteChars(10)
   .start();
